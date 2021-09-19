16d3a59afeb2acab52b17873cf3e7429d0f24d38
Sarah Marie Azzopardi <sarahazzopardi17@hotmail.com>
2021-09-19 10:47:55 +0200

    fixing code

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+import Cart from "../components/Cart";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
-} from '../utils/actions';
-import { QUERY_PRODUCTS } from '../utils/queries';
-import { idbPromise } from '../utils/helpers';
-import spinner from '../assets/spinner.gif';
+} from "../utils/actions";
+import { QUERY_PRODUCTS } from "../utils/queries";
+import { idbPromise } from "../utils/helpers";
+import spinner from '../assets/spinner.gif'
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
@@ -27,13 +28,13 @@ function Detail() {
   useEffect(() => {
     // already in global store
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
+      setCurrentProduct(products.find(product => product._id === id));
     }
     // retrieved from server
     else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products,
+        products: data.products
       });
 
       data.products.forEach((product) => {
@@ -45,37 +46,38 @@ function Detail() {
       idbPromise('products', 'get').then((indexedProducts) => {
         dispatch({
           type: UPDATE_PRODUCTS,
-          products: indexedProducts,
+          products: indexedProducts
         });
       });
     }
   }, [products, data, loading, dispatch, id]);
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
       idbPromise('cart', 'put', {
         ...itemInCart,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...currentProduct, purchaseQuantity: 1 },
+        product: { ...currentProduct, purchaseQuantity: 1 }
       });
       idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+
     }
-  };
+  }
 
   const removeFromCart = () => {
     dispatch({
       type: REMOVE_FROM_CART,
-      _id: currentProduct._id,
+      _id: currentProduct._id
     });
 
     idbPromise('cart', 'delete', { ...currentProduct });
@@ -85,17 +87,25 @@ function Detail() {
     <>
       {currentProduct && cart ? (
         <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+          <Link to="/">
+            ← Back to Products
+          </Link>
 
           <h2>{currentProduct.name}</h2>
 
-          <p>{currentProduct.description}</p>
+          <p>
+            {currentProduct.description}
+          </p>
 
           <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button onClick={addToCart}>Add to Cart</button>
+            <strong>Price:</strong>
+            ${currentProduct.price}
+            {" "}
+            <button onClick={addToCart}>
+              Add to Cart
+            </button>
             <button
-              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              disabled={!cart.find(p => p._id === currentProduct._id)}
               onClick={removeFromCart}
             >
               Remove from Cart
@@ -108,10 +118,12 @@ function Detail() {
           />
         </div>
       ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      {
+        loading ? <img src={spinner} alt="loading" /> : null
+      }
       <Cart />
     </>
   );
-}
+};
 
 export default Detail;
--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+import { idbPromise } from "../utils/helpers";
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
@@ -10,7 +10,7 @@ function Success() {
   useEffect(() => {
     async function saveOrder() {
       const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
+      const products = cart.map(item => item._id);
 
       if (products.length) {
         const { data } = await addOrder({ variables: { products } });
@@ -20,24 +20,26 @@ function Success() {
           idbPromise('cart', 'delete', item);
         });
       }
-
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
     }
 
     saveOrder();
   }, [addOrder]);
 
+  setTimeout(function () { window.location.assign("/") }, 3000);
+
   return (
     <div>
       <Jumbotron>
         <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
+        <h2>
+          Thank you for your purchase!
+                </h2>
+        <h2>
+          You will now be redirected to the homepage.
+                </h2>
       </Jumbotron>
     </div>
   );
-}
+};
 
 export default Success;
