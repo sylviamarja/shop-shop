e35cfa59b8b647189086de12296bd7a33e29be41
sylviamarja <82944649+sylviamarja@users.noreply.github.com>
2021-09-20 12:29:55 +0200

    fixing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://azzopars:<password>@cluster0.7rzs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://azzopars:ZoaLp24qGfokNx9s@cluster0.7rzs3.mongodb.net/shopshop?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
