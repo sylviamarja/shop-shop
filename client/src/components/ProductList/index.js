8b27e5895e2af65e4b44a724781cbc3d82cc0c08
sylviamarja <82944649+sylviamarja@users.noreply.github.com>
2021-09-20 05:50:15 +0200

    fixining broken code

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,52 +1,46 @@
 import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
+
+import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
+import { updateProducts } from '../../app/actions/actions';
+import { idbPromise } from '../../utils/helpers';
+import { useSelector, useDispatch } from 'react-redux';
 
-function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
-  const { currentCategory } = state;
+const ProductList = () => {
+  const dispatch = useDispatch();
+  const { currentCategory, products } = useSelector((state) => state);
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
     if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
+      dispatch(updateProducts(data.products));
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
     } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
       idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
-        });
+        // use retrieved data to set global state for offline browsing
+        dispatch(updateProducts(products));
       });
     }
-  }, [data, loading, dispatch]);
+  }, [loading, data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return state.products;
+      return data.products;
     }
 
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
-    );
+    return data.products.filter(product => product.category._id === currentCategory);
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
